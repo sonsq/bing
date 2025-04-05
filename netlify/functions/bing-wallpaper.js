25c9dfd69e77824e774e4b44c11275ebc24abd3b
sonsq <83900858+sonsq@users.noreply.github.com>
2025-04-05 14:07:13 +0800

    Create bing-wallpaper.js

---
+++ netlify/functions/bing-wallpaper.js
@@ -0,0 +1,22 @@
+exports.handler = async (event, context) => {
+    try {
+      const response = await fetch('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US');
+      const data = await response.json();
+      
+      const imageUrl = `https://www.bing.com${data.images[0].url}`;
+      
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          url: imageUrl,
+          copyright: data.images[0].copyright
+        }),
+        headers: {'Content-Type': 'application/json'}
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({error: 'Failed to fetch wallpaper'})
+      };
+    }
+  };
