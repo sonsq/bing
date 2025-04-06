130c19690643cdbbcc4b53dc871eac18f8771cb4
sonsq <83900858+sonsq@users.noreply.github.com>
2025-04-06 11:14:45 +0800

    Update bing-wallpaper.js

--- netlify/functions/bing-wallpaper.js
+++ netlify/functions/bing-wallpaper.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
   try {
-    const response = await fetch('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN');
+    const response = await fetch('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=jp-JA');
     const data = await response.json();
     
     const imageUrl = `https://www.bing.com${data.images[0].url}`;
